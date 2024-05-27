3dbdfb2c364652bbc65f4cdd6b42cb81f8fc8a16
SEBASTIAN GUZMAN SOTO <20200981@aloe.ulima.edu.pe>
2024-05-26 23:23:51 -0500

    BackEndMedico: Metodos Agregar

--- backend/src/medicos/medicos.controller.ts
+++ backend/src/medicos/medicos.controller.ts
@@ -1,6 +1,8 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { MedicosService } from './medicos.service';
 import { CrearRecetaMedicaDto } from 'src/recetas-medicas/dto/recetas-medicas.dto';
+import { CrearImagenMedicaDto } from 'src/imagenes-medicas/dto/imagenes-medicas.dto';
+import { CrearResultadoLabDto } from 'src/resultados-lab/dto/resultados-lab.dto';
 
 @Controller('medicos')
 export class MedicosController {
@@ -26,7 +28,7 @@ export class MedicosController {
         return this.medicoService.VisualizacionResultadosLaboratorio(idpac,idmed)
     }
 
-    @Post(':idcita/:IDMedico')
+    @Post('AgregarReceta/:idcita/:IDMedico')
     agregarRecetaMedica(
         @Body() nuevaReceta: CrearRecetaMedicaDto, 
         @Param('idcita') idcita: String,
@@ -34,4 +36,23 @@ export class MedicosController {
     ){
         return this.medicoService.AgregarRecetaMedica(nuevaReceta.observacion,nuevaReceta.medicamento, idcita, IDMedico)
     }
+
+    @Post('AgregarImagen/:idcita/:IDMedico')
+    agregarImagenMedica(
+        @Body() nuevaImagen: CrearImagenMedicaDto,
+        @Param('idcita') idcita: String,
+        @Param('IDMedico') IDMedico: String
+    ){
+        return this.medicoService.AgregarImagenMedica(idcita, IDMedico, nuevaImagen.tipo, nuevaImagen.imagen, nuevaImagen.nombrePaciente)
+    }
+
+    @Post('AgregarResultadoLab/:idcita/:IDMedico')
+    agregarResultadoLab(
+        @Body() nuevoRes: CrearResultadoLabDto,
+        @Param('idcita') idcita: String,
+        @Param('IDMedico') IDMedico: String
+    ){
+        return this.medicoService.AgregarResultadoLab(idcita, IDMedico, nuevoRes.tipo, nuevoRes.resultado, nuevoRes.Nombrepaciente)
+    }
+
 }
