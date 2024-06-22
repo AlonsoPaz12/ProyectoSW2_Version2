c2e79d2fba09e1562ae42db3e851e36ecea0322d
GiaC <gacame1501@gmail.com>
2024-06-21 21:11:17 -0500

    cambio

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,21 +1,10 @@
+// main.ts
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  const corsOptions: CorsOptions = {
-    origin: 'http://localhost:3001',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
-  }
-
-  app.enableCors(corsOptions);
-
+  app.enableCors(); // Habilitar CORS
   await app.listen(3000);
 }
-
-
 bootstrap();
